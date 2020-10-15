50b12c429511d0c247017b9d835f24bd30906aa0
emfauzanasharii <asharifauzan29@gmail.com>
2020-10-15 12:46:19 +0700

    cara menggunakan API

--- application/models/M_jadwal.php
+++ application/models/M_jadwal.php
@@ -16,7 +16,13 @@ class M_jadwal extends CI_Model{
 	}
 
 	 public function get_search($search){
-		$hsl=$this->db->query("SELECT * FROM tbl_jadwal where jadwal_hari='$search' or jadwal_terminal_awal='$search' or jadwal_terminal_tujuan='$search' or jadwal_bus_id='$search'");
+		$hsl=$this->db->query("SELECT  jadwal_id, bus_nama, hari_nama, terminal_nama as asal,terminal_nama as tujuan, jadwal_jam_berangkat, jadwal_jam_sampai,jadwal_terminal_tujuan as tujuan
+			FROM tbl_jadwal as jdw
+			join tbl_bus on jadwal_bus_id=bus_id
+			join tbl_hari on jadwal_hari=hari_id
+			join tbl_terminal as A on jdw.jadwal_terminal_awal=A.terminal_id
+			-- join tbl_terminal as B on tbl_jadwal.jadwal_terminal_tujuan=B.terminal_id
+			 where jadwal_hari='$search' or jadwal_terminal_awal='$search' or jadwal_terminal_tujuan='$search' or jadwal_bus_id='$search'");
 		return $hsl->result();
   }
 
